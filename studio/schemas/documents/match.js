07b6050baac28bcb33b8b5aca6a565ea38262967
Jacob Thompson <thampsen@gmail.com>
2021-01-28 18:05:41 +0000

    add example local schemas

---
+++ studio/schemas/documents/match.js
@@ -0,0 +1,17 @@
+export default {
+  name: "match",
+  type: "document",
+  title: "Match",
+  fields: [
+    {
+      name: "date",
+      type: "date",
+      title: "Date",
+    },
+    {
+      name: "provider",
+      type: "string",
+      title: "Provider",
+    }
+  ]
+}
---
+++ studio/schemas/documents/provider.js
@@ -0,0 +1,17 @@
+export default {
+  name: "provider",
+  type: "document",
+  title: "Provider",
+  fields: [
+    {
+      name: "name",
+      title: "Provider Name",
+      type: "string"
+    },
+    {
+      name: "image",
+      title: "Provider Image",
+      type: "image"
+    }
+  ]
+}
